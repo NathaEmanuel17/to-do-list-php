a8ce29d8dc5fc94a81709d1d4456714f26bba6e4
MNVP Emanuel <nathaemanuel.eng@gmail.com>
2022-07-28 09:38:48 -0300

    first commit

---
+++ to-do-php-puro/conexao.php
@@ -0,0 +1,28 @@
+<?php
+
+class Conexao {
+
+	private $host = 'localhost';
+	private $dbname = 'to_do_list';
+	private $user = 'root';
+	private $pass = 'root';
+
+	public function conectar() {
+		try {
+
+			$conexao = new PDO(
+				"mysql:host=$this->host;dbname=$this->dbname",
+				"$this->user",
+				"$this->pass"				
+			);
+
+			return $conexao;
+
+
+		} catch (PDOException $e) {
+			echo '<p>'.$e->getMessage().'</p>';
+		}
+	}
+}
+
+?>
